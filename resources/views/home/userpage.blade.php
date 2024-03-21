2a9e46a80a078452d8b2e1b5706110d112907c2f
Meshgenerali <meshackmuuo595@gmail.com>
2024-03-21 09:08:24 +0300

    debugging

--- resources/views/home/userpage.blade.php
+++ resources/views/home/userpage.blade.php
@@ -37,7 +37,7 @@
 
             <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
                  
-                  <span class="block sm:inline" id="alertMessage">Your browser doesn't support WebAuthn.</span>
+                  <span class="block sm:inline" id="alertMessage"></span>
 
             </div>
          </div>
